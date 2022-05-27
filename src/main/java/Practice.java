01e2b76028f958d8f85671b077941d5c584c9557
saikoppi <swaroopkoppisetty@gmail.com>
2022-05-27 12:58:48 +0530

    27th May

--- src/main/java/Practice.java
+++ src/main/java/Practice.java
@@ -1,28 +1,15 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Practice {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
 
-            int arr[] = new int[]{2, 9, 8, 4, 6, 1, 7, 5, 3, 0};
-
-            for (int i = 0; i < arr.length; i++) {
-
-                arr[i / 2] = arr[arr[arr[i / 3] / 3] / 3];
-
-            }
-
-
-            for (int i = 0; i < arr.length; i++)
-
-                System.out.print(arr[i] + " ");
-
+        System.out.println(LocalDateTime.now());
 
 
     }
-
-
 }
